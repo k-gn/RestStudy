e186d57cede856984df63f71072dc283ecd07b88
k-gn <rlarbska97@gmail.com>
2021-10-01 16:31:10 +0900

    no message

---
+++ data-rest/src/main/java/com/example/getinline/repository/AdminRepository.java
@@ -0,0 +1,6 @@
+package com.example.getinline.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
--- data-rest/src/main/java/com/example/getinline/repository/EventRepository.java
+++ data-rest/src/main/java/com/example/getinline/repository/EventRepository.java
@@ -2,15 +2,32 @@ package com.example.getinline.repository;
 
 import com.example.getinline.constant.EventStatus;
 import com.example.getinline.domain.Event;
+import com.example.getinline.domain.QEvent;
 import com.example.getinline.dto.EventDTO;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-// TODO: 인스턴스 생성 편의를 위해 임시로 default 사용. repository layer 구현이 완성되면 삭제할 것
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends
+        JpaRepository<Event, Long>,
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QEvent root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.placeId, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
+        bindings.bind(root.eventName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
+        bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
+    }
 
     default List<EventDTO> findEvents(
             Long placeId,
